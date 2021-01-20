c77cd5fb9b0881d7edebe932a0ac4754cf655f5a
burakpols <burakpolsq@gmail.com>
2021-01-20 18:06:34 +0300

    Proje dosyası ekle.

---
+++ ClassMetodDemo/MusteriManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetodDemo
+{
+    public class MusteriManager
+    {
+        
+       
+    public static Musteri MusteriEkle(int id, string isim, string soyisim, int yas)
+        {
+            
+            Musteri musteri = new Musteri();
+            musteri.id = id;
+            musteri.isim = isim;
+            musteri.soyisim = soyisim;
+            musteri.yas = yas;
+            
+            return musteri;
+            
+           
+        }
+        public static void MusteriListele(Musteri musteri)
+
+        {
+            Console.WriteLine(musteri.isim);
+            Console.WriteLine(musteri.soyisim);
+            Console.WriteLine("************************");
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ClassMetodDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Müşteri ekleme
+            Musteri musteri1= MusteriManager.MusteriEkle(1, "Burak", "Akay", 24);
+            Musteri musteri2 = MusteriManager.MusteriEkle(2, "Mahmut", "Akay", 34);
+            Musteri musteri3 = MusteriManager.MusteriEkle(3, "Gizem", "Akay", 15);
+            Musteri musteri4 = MusteriManager.MusteriEkle(4, "Lale", "Akay", 21);
+            //Müşterileri listeleme
+            MusteriManager.MusteriListele(musteri1);
+            MusteriManager.MusteriListele(musteri2);
+            MusteriManager.MusteriListele(musteri3);
+            MusteriManager.MusteriListele(musteri4);
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
